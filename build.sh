34ec7f3b4cc300ec0304f2adc607233322208972
Chris Watts <Chris@CJWatts.com>
2013-03-15 16:28:49 +0000

    Update build files

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-cd /d %~dp0
+#!/bin/bash
+cd "$(dirname "$0")"
 cd src
 javac -cp ".;../lib/guava-11.0.2.jar;../lib/javassist-3.12.1.GA.jar;../lib/reflections-0.9.8.jar" com/cjwatts/fractalexplorer/main/FractalExplorer.java
-cd ..\bin
+cd ../bin
 java -cp ".;../lib/guava-11.0.2.jar;../lib/javassist-3.12.1.GA.jar;../lib/reflections-0.9.8.jar" com/cjwatts/fractalexplorer/main/FractalExplorer
